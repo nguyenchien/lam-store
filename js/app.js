81aaec7321c2ab0acd8c2ee92a9c98f4fb653241
Chien Nguyen <chiennguyen1702@gmail.com>
2025-10-17 14:25:12 +0700

    footer

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Loader
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", () => {
   const loader = document.querySelector(".loader-wrapper");
   setTimeout(() =>  {
     loader.classList.add("is-done");
@@ -7,7 +7,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 // Scroll to top
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", () => {
   const scrollBtn = document.querySelector('.js-scroll-to-top');
 
   // Khi click vào nút -> scroll lên đầu
@@ -17,4 +17,26 @@ window.addEventListener("DOMContentLoaded", (event) => {
       behavior: "smooth" // Cuộn mượt
     });
   });
+});
+
+// Click js-menu-button
+document.addEventListener("DOMContentLoaded", () => {
+  const menuNav = document.querySelector(".js-menu");
+  const menuButton = document.querySelector(".js-menu-button");
+  const menuOverlay = document.querySelector(".l-menu-overlay");
+
+  if (menuNav && menuButton && menuOverlay) {
+    menuButton.addEventListener("click", () => {
+      menuNav.classList.toggle("is-open");
+      menuButton.classList.toggle("is-open");
+      menuOverlay.classList.toggle("is-open");
+    });
+
+    // Khi click vào overlay thì đóng menu
+    menuOverlay.addEventListener("click", () => {
+      menuNav.classList.remove("is-open");
+      menuButton.classList.remove("is-open");
+      menuOverlay.classList.remove("is-open");
+    });
+  }
 });
